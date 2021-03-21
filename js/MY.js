4878564c48f245d8fc9838c0f2b5ba479417dbb4
Lopez Mark <leon51414@icloud.com>
2021-03-22 02:05:19 +0900

    section12card編集完成もう一個はこれから

--- js/MY.js
+++ js/MY.js
@@ -2,3 +2,12 @@
 $(document).ready(function() {
     $('.drawer').drawer();
 });
+
+//ドロップダウンメニュー
+new Vue({
+    el: '#app',
+    data:{
+        isActive1:false,
+        isActive2:false,
+    },
+    })
